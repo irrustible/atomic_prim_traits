ba8fcc24e4693a48d6f65ccacfa51a240783f97c
Tim Nieradzik <tim@sparse.tech>
2023-09-14 18:13:17 +0300

    Override Output of primitive traits and add test case

--- src/lib.rs
+++ src/lib.rs
@@ -14,17 +14,17 @@ use std::ops::{
 pub trait AtomicInt : Default + Send + Sync + RefUnwindSafe + UnwindSafe {
     type Prim
         : Copy + Debug + Display + Eq + Hash + Ord + Sized
-        + Add + AddAssign
-        + BitAnd + BitAndAssign
-        + BitOr + BitOrAssign
-        + BitXor + BitXorAssign
-        + Div + DivAssign
-        + Mul + MulAssign
-        + Not
-        + Rem + RemAssign
-        + Shl + ShlAssign
-        + Shr + ShrAssign
-        + Sub + SubAssign;
+        + Add<Output = <Self as AtomicInt>::Prim> + AddAssign
+        + BitAnd<Output = <Self as AtomicInt>::Prim> + BitAndAssign
+        + BitOr<Output = <Self as AtomicInt>::Prim> + BitOrAssign
+        + BitXor<Output = <Self as AtomicInt>::Prim> + BitXorAssign
+        + Div<Output = <Self as AtomicInt>::Prim> + DivAssign
+        + Mul<Output = <Self as AtomicInt>::Prim> + MulAssign
+        + Not<Output = <Self as AtomicInt>::Prim>
+        + Rem<Output = <Self as AtomicInt>::Prim> + RemAssign
+        + Shl<Output = <Self as AtomicInt>::Prim> + ShlAssign
+        + Shr<Output = <Self as AtomicInt>::Prim> + ShrAssign
+        + Sub<Output = <Self as AtomicInt>::Prim> + SubAssign;
 
     const ZERO: <Self as AtomicInt>::Prim;
     const ONE: <Self as AtomicInt>::Prim;
@@ -262,3 +262,28 @@ impl_atomic_int!(atomic::AtomicI16 = i16);
 impl_atomic_int!(atomic::AtomicI32 = i32);
 impl_atomic_int!(atomic::AtomicI64 = i64);
 impl_atomic_int!(atomic::AtomicIsize = isize);
+
+#[cfg(test)]
+mod test {
+    use std::sync::atomic::AtomicU8;
+    use super::*;
+
+    #[test]
+    fn test_fetch_update() {
+        fn incr<T: AtomicInt>(value: &T) -> Result<<T as AtomicInt>::Prim, <T as AtomicInt>::Prim> {
+            value.fetch_update(
+                Ordering::SeqCst,
+                Ordering::SeqCst,
+                |i| Some(if i == T::MAX { T::MIN } else { i + T::ONE })
+            )
+        }
+
+        let value = AtomicU8::new(0);
+        assert_eq!(incr(&value), Ok(0));
+        assert_eq!(incr(&value), Ok(1));
+
+        let value = AtomicU8::new(255);
+        assert_eq!(incr(&value), Ok(255));
+        assert_eq!(incr(&value), Ok(0));
+    }
+}
